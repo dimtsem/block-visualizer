41aef606426acd03d1ba2c38d2f3480ab45cad03
Unknown <dtsement@princeton.edu>
2018-04-10 19:55:32 -0400

    add some things to about.html, add input in index.html, and add equality tests

--- app.py
+++ app.py
@@ -44,58 +44,198 @@ def create_figure(G,ntx):
 
     plot.renderers.append(graph_renderer)
 
-
     return plot
+
+def create_transactor_figure(G):
+    plot = Plot(plot_width=800, plot_height=600,
+            x_range=Range1d(-1.1,1.1), y_range=Range1d(-1.1,1.1))
+    plot.title.text = "Transactor Visualization"
+
+    plot.add_tools(HoverTool(tooltips=None), TapTool(), BoxSelectTool(), WheelZoomTool(), PanTool())
+
+    graph_renderer = from_networkx(G, nx.spring_layout, scale=1, center=(0,0))
+
+    graph_renderer.node_renderer.glyph = Circle(size=4, fill_color=Spectral4[0])
+    graph_renderer.node_renderer.selection_glyph = Circle(size=4, fill_color=Spectral4[2])
+    graph_renderer.node_renderer.hover_glyph = Circle(size=4, fill_color=Spectral4[1])
+
+    graph_renderer.edge_renderer.glyph = MultiLine(line_color="#CCCCCC", line_alpha=0.8, line_width=3)
+    graph_renderer.edge_renderer.selection_glyph = MultiLine(line_color=Spectral4[2], line_width=3)
+    graph_renderer.edge_renderer.hover_glyph = MultiLine(line_color=Spectral4[1], line_width=3)
+
+    graph_renderer.selection_policy = NodesAndLinkedEdges()
+    graph_renderer.inspection_policy = EdgesAndLinkedNodes()
+
+    plot.renderers.append(graph_renderer)
+
+    return plot	
+
+def get_wallet_data(wallethash):
+    w = str(wallethash)
+    wallet_url = 'https://blockchain.info/rawaddr/'+w
+    data = requests.get(wallet_url)
+    wallet = data.json()
+    return pd.DataFrame(wallet)['txs']
+
+def wallet_filter(wallethash,Z):
+    for i in Z.index:
+      if wallethash in Z['Receivers'][i]:
+        Z['Receivers'][i] = [wallethash]
+      elif wallethash in Z['Senders'][i]:
+        Z['Senders'][i] = [wallethash]
+      else:
+        pass
+    return Z
+
+def get_nodes(wallet):
+    txdata = get_wallet_data(wallet)
+    n_tx = len(txdata)
+    tx = pd.DataFrame(txdata.tolist())
+    LL = [[tx['inputs'][j][i]['prev_out']['addr'] for i in range(tx['vin_sz'][j])] for j in range(0,n_tx)]
+    txin = pd.Series(LL)
+    LLL = [[tx['out'][j][i]['addr'] for i in range(tx['vout_sz'][j]) if 'addr' in tx['out'][j][i].keys()] for j in range(0,n_tx)]
+    txout = pd.Series(LLL)
+    txg_nodes = pd.DataFrame({'Senders':txin,'Receivers':txout})
+    W = wallet_filter(wallet, txg_nodes)
+    return W
+
+def get_interactors(wallet):
+    Z = get_nodes(wallet)
+    reclist = [z for y in [x for x in Z['Receivers']] for z in y]
+    senlist = [z for y in [x for x in Z['Senders']] for z in y]
+    return list(set(reclist+senlist))
+
+def get_interactors_ofdepth(wallet,depth):
+    if depth == 0:
+      X = get_interactors(wallet)
+      return X
+    else: 
+      Y = []
+      for interactor in get_interactors_ofdepth(wallet, depth-1):
+        Z = get_interactors(interactor)
+        for w in Z:
+          Y.append(w)
+      return Y
+
+def make_graph(nodedata):
+    G = nx.MultiDiGraph()
+    for i in nodedata.index:
+      for x in nodedata['Senders'][i]:
+        for y in nodedata['Receivers'][i]:
+          G.add_edge(x,y)
+    return G
+
+def make_graph_ofdepth(wallet,depth):
+    if depth == 0:
+      G = make_graph(get_nodes(wallet))
+      return G
+    else:
+      G = make_graph_ofdepth(wallet,depth-1)
+      for interactor in get_interactors_ofdepth(wallet, depth):
+        nodedata = get_nodes(interactor)
+        for i in nodedata.index:
+          for x in nodedata['Senders'][i]:
+            for y in nodedata['Receivers'][i]:
+              G.add_edge(x,y)
+    return G
+
+
+########################
+#### Equality Tests #### 
+########################
+
+def get_nodes_nofilter(wallet):
+    txdata = get_wallet_data(wallet)
+    n_tx = len(txdata)
+    tx = pd.DataFrame(txdata.tolist())
+    LL = [[tx['inputs'][j][i]['prev_out']['addr'] for i in range(tx['vin_sz'][j])] for j in range(0,n_tx)]
+    txin = pd.Series(LL)
+    LLL = [[tx['out'][j][i]['addr'] for i in range(tx['vout_sz'][j]) if 'addr' in tx['out'][j][i].keys()] for j in range(0,n_tx)]
+    txout = pd.Series(LLL)
+    return pd.DataFrame({'Senders':txin,'Receivers':txout})
+
+def equalitytest_simple(wallet1, wallet2):
+    senders1 = get_nodes_nofilter(wallet1)['Senders']
+    senders2 = get_nodes_nofilter(wallet2)['Receivers']
+    while True:
+        for x in senders1:
+            if wallet2 in x:
+                return True
+            else:
+                pass
+        for y in senders2:
+            if wallet1 in y:
+                return True
+            else:
+                pass
+        break
+    return False
 
 @app.route('/')
 def index():
-#    return render_template('index2.html')
-
-#@app.route('/block_plot')
-#def figure():
-  blockhash = request.args.get("blockhash")
   
-  if blockhash == 'latest':
-    #Retrieve the latest bloc hash from the Blockchain.info API
-    latesthash = 'https://blockchain.info/q/latesthash'
-    r = requests.get(latesthash)
-    lh = str(r.text)
-    latestblock_url = 'https://blockchain.info/rawblock/'+lh
-    data = requests.get(latestblock_url)
-    block = data.json()
-  else:
-    try:
+  blockhash = request.args.get("blockhash")
+  wallethash = request.args.get("wallethash")
+  #print(blockhash, type(blockhash))
+  #print(wallethash, type(wallethash))
+
+  if blockhash != None:
+    if blockhash == 'latest':
+      #Retrieve the latest bloc hash from the Blockchain.info API
+      latesthash = 'https://blockchain.info/q/latesthash'
+      r = requests.get(latesthash)
+      lh = str(r.text)
+      latestblock_url = 'https://blockchain.info/rawblock/'+lh
+      data = requests.get(latestblock_url)
+      block = data.json()
+    else:
+      try:
         latestblock_url = 'https://blockchain.info/rawblock/'+blockhash
         data = requests.get(latestblock_url)
-        block = data.json()
-    except:
+        block = data.json()	
+      except:
         print('We could not retrieve block - incorrect block hash?')
         return render_template('index2.html')
- 
-  #Convert the data into a DataFrame
-  n_tx = int(block['n_tx'])
-  txseries = pd.Series(block['tx'])
-  tx = pd.DataFrame(txseries.tolist())
-  LL = [[tx['inputs'][j][i]['prev_out']['addr'] for i in range(tx['vin_sz'][j])] for j in range(1,n_tx)]
-  txin = pd.Series(LL)
-  LLL = [[tx['out'][j][i]['addr'] for i in range(tx['vout_sz'][j]) if 'addr' in tx['out'][j][i].keys()] for j in range(0,n_tx)]
-  txout = pd.Series(LLL)
-  txin.loc[-1]= ['N/A']
-  txin.index = txin.index + 1
-  txin = txin.sort_index()
-  txg_nodes = pd.DataFrame({'Senders':txin,'Receivers':txout})
-    
-  #Turn the DataFrame into a networkx directed multigraph
-  G = nx.MultiDiGraph()
-  for i in range(n_tx):
-    for x in txg_nodes['Senders'][i]:
-        for y in txg_nodes['Receivers'][i]:
-            G.add_edge(x,y)
-  
-  plot = create_figure(G,n_tx)
+    #Convert the data into a DataFrame
+    n_tx = int(block['n_tx'])
+    txseries = pd.Series(block['tx'])
+    tx = pd.DataFrame(txseries.tolist())
+    LL = [[tx['inputs'][j][i]['prev_out']['addr'] for i in range(tx['vin_sz'][j])] for j in range(1,n_tx)]
+    txin = pd.Series(LL)
+    LLL = [[tx['out'][j][i]['addr'] for i in range(tx['vout_sz'][j]) if 'addr' in tx['out'][j][i].keys()] for j in range(0,n_tx)]
+    txout = pd.Series(LLL)
+    txin.loc[-1]= ['N/A']
+    txin.index = txin.index + 1
+    txin = txin.sort_index()
+    txg_nodes = pd.DataFrame({'Senders':txin,'Receivers':txout})
 
-  script, div = components(plot)
-  return render_template("block_plot.html", script=script, div=div)
+    #Turn the DataFrame into a networkx directed multigraph
+    G = nx.MultiDiGraph()
+    for i in range(n_tx):
+      for x in txg_nodes['Senders'][i]:
+        for y in txg_nodes['Receivers'][i]:
+          G.add_edge(x,y)
+
+    plot = create_figure(G,n_tx)
+
+    script, div = components(plot)
+    return render_template("block_plot.html", script=script, div=div)
+  elif wallethash != None:
+    #try:
+      G = make_graph_ofdepth(get_nodes(wallethash),1)
+      print('Graph OK')
+      plot = create_transactor_figure(G)
+      print('plot OK')
+      script, div = components(plot)
+      print('script, div OK')
+      return render_template("wallet_plot.html", script=script, div=div)
+      print('OK')
+    #except:
+    #  print('We could not retrieve wallet - incorrect wallet hash?')
+    #  return render_template('index2.html')
+  else:
+      print('Input makes no sense.')
+      return render_template('index2.html')
 
 
 @app.route('/about')
